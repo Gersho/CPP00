99f9d87beda0272b5d7f5f4c7823f99b852cfa7f
Gersho <zennoune.karim@outlook.fr>
2021-09-08 21:00:00 +0200

    ex02 skeleton

---
+++ ex02/Account.cpp
@@ -0,0 +1,73 @@
+// ************************************************************************** //
+//                                                                            //
+//                Account.cpp for GlobalBanksters United                      //
+//                Created on  : Wed Sep 08 20:55:17 2021                      //
+//                Last update : Wed Sep 08 21:23:52 2021                      //
+//                Made by : Karim "kzennoun" Zennoune <kz@gbu.com>            //
+//                                                                            //
+// ************************************************************************** //
+
+#include "Account.hpp"
+
+int	Account::getNbAccounts( void )
+{
+
+}
+
+int	Account::getTotalAmount( void )
+{
+
+}
+
+int	Account::getNbDeposits( void )
+{
+
+}
+
+int	Account::getNbWithdrawals( void )
+{
+
+}
+
+void	Account::displayAccountsInfos( void )
+{
+
+}
+
+
+Account::Account( int initial_deposit )
+{
+
+}
+
+Account::~Account( void )
+{
+
+}
+
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
+
+
+void	Account::_displayTimestamp( void )
+{
+
+}
